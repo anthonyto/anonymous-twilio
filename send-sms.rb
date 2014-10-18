4656d5243102c0f6c9c9d223f96ddebd7a3a34c9
alexfaber2011 <alexfaber2011@gmail.com>
2014-10-18 16:16:52 -0500

    send texts anonymously (AGAIN)

--- send-sms.rb
+++ send-sms.rb
@@ -11,32 +11,8 @@ configure do
   Mongoid.load!('mongoid.yml')
 end
 
-# Credentials
-account_sid = "AC5e0e37adaf556c3ef136e9ba71536c74"
-auth_token  = "f5ca2a5df3edfbf3c1f81014d11de97b"
-client      = Twilio::REST::Client.new account_sid, auth_token
-from        = "+17606421123" # Your Twilio number
-
- 
-friends = {
-"+18582295512" => "Anthony",
-"+17155737579" => "Alex",
-"+17159372022" => "Nick"
-}
-
-
-# friends.each do |key, value|
-#   client.account.messages.create(
-#     :from => from,
-#     :to => key,
-#     :body => "Hey #{value}, Monkey party at 6PM. Bring Bananas!"
-#   )
-#   puts "Sent message to #{value}"
-# end
-
 #Creat new app of type Sinatra
 class MyApp < Sinatra::Base
-
   # Twilio Ruby Gem Credentials
   account_sid = "AC5e0e37adaf556c3ef136e9ba71536c74"
   auth_token  = "f5ca2a5df3edfbf3c1f81014d11de97b"
@@ -61,8 +37,11 @@ class MyApp < Sinatra::Base
     fromNumber = params[:From]
     body = params[:Body]
 
-    puts body
+    puts "========= BEGIN - RESPONSE FROM SOMEONE WHO DOESN'T KNOW WHAT'S GOING ON ==========="
     puts fromNumber
+    puts body
+    puts "========= END  -  RESPONSE FROM SOMEONE WHO DOESN'T KNOW WHAT'S GOING ON ==========="
+
 
     toNumber = body.split(" ")[0]
     body.slice! /^\S+\s+/
@@ -76,8 +55,5 @@ class MyApp < Sinatra::Base
         :to => toNumber,
         :body => body
     )
-
-
   end
-  
 end
