7a3c00f44dd82306706622d9f11bde65793a2a5d
alexfaber2011 <alexfaber2011@gmail.com>
2014-10-18 14:29:45 -0500

    playing around with mongo

--- send-sms.rb
+++ send-sms.rb
@@ -1,6 +1,8 @@
 require 'rubygems'
 require 'sinatra'
 require 'twilio-ruby'
+require 'mongo'
+require 'bson_ext'
 
 # Credentials
 account_sid = "AC5e0e37adaf556c3ef136e9ba71536c74"
@@ -9,9 +11,9 @@ client      = Twilio::REST::Client.new account_sid, auth_token
 from        = "+17606421123" # Your Twilio number
 
  
-friends = {=
-"+18582295512" => "Anthony", 
-"+17155737579" => "Alex", 
+friends = {
+"+18582295512" => "Anthony",
+"+17155737579" => "Alex",
 "+17159372022" => "Nick"
 }
 
@@ -23,4 +25,11 @@ friends.each do |key, value|
     :body => "Hey #{value}, Monkey party at 6PM. Bring Bananas!"
   )
   puts "Sent message to #{value}"
+end
+
+get '/sms-sent' do
+  sender = params[:From]
+  puts params
+
+  twiml = Twill
 end
