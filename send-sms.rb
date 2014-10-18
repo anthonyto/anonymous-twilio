26545dd4490791b14d46e085ed28e183b50ffa92
alexfaber2011 <alexfaber2011@gmail.com>
2014-10-18 14:59:02 -0500

    playing around with mongo

--- send-sms.rb
+++ send-sms.rb
@@ -2,7 +2,15 @@ require 'rubygems'
 require 'sinatra'
 require 'twilio-ruby'
 require 'mongo'
-require 'bson_ext'
+require 'json/ext'
+
+include Mongo
+
+configure do
+  conn = MongoClient.new("mongodb://anon:anonymous@linus.mongohq.com:10077/anonymous_twillio")
+  set :mongo_connection, conn
+  # set :mongo_db, conn.db('anonymous_twillio')
+end
 
 # Credentials
 account_sid = "AC5e0e37adaf556c3ef136e9ba71536c74"
@@ -10,7 +18,6 @@ auth_token  = "f5ca2a5df3edfbf3c1f81014d11de97b"
 client      = Twilio::REST::Client.new account_sid, auth_token
 from        = "+17606421123" # Your Twilio number
 
- 
 friends = {
 "+18582295512" => "Anthony",
 "+17155737579" => "Alex",
@@ -18,18 +25,20 @@ friends = {
 }
 
 
-friends.each do |key, value|
-  client.account.messages.create(
-    :from => from,
-    :to => key,
-    :body => "Hey #{value}, Monkey party at 6PM. Bring Bananas!"
-  )
-  puts "Sent message to #{value}"
-end
+# friends.each do |key, value|
+#   client.account.messages.create(
+#     :from => from,
+#     :to => key,
+#     :body => "Hey #{value}, Monkey party at 6PM. Bring Bananas!"
+#   )
+#   puts "Sent message to #{value}"
+# end
 
-get '/sms-sent' do
-  sender = params[:From]
-  puts params
-
-  twiml = Twill
-end
+put settings.mongo_db["sms"].find.to_a.to_json
+#
+# get '/sms-sent' do
+#   sender = params[:From]
+#   puts params
+#
+#   twiml = Twill
+# end
